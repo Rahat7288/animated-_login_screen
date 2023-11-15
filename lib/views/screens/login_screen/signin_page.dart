65e2168d74917cc50478ca8127921bcc7a19f650
Rahat Islam Akash <rahatislamakash@gmail.com>
2023-11-15 22:09:20 +0600

    my escape plan

--- lib/views/screens/login_screen/signin_page.dart
+++ lib/views/screens/login_screen/signin_page.dart
@@ -9,68 +9,181 @@ class LoginScreen extends StatelessWidget {
     return Scaffold(
       backgroundColor: Colors.white,
       body: SingleChildScrollView(
-        child: Container(
-          child: Column(
-            children: [
-              Container(
-                height: 400,
-                decoration: BoxDecoration(
-                  image: DecorationImage(
-                    image: AssetImage('assets/images/background.png'),
-                    fit: BoxFit.fill,
-                  ),
+        child: Column(
+          children: [
+            Container(
+              height: 400,
+              decoration: const BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage('assets/images/background.png'),
+                  fit: BoxFit.fill,
                 ),
-                child: Stack(
-                  children: [
-                    Positioned(
-                      left: 30,
-                      width: 80,
-                      height: 200,
-                      child: FadeInUp(
-                        duration: Duration(seconds: 1),
-                        child: Container(
-                          decoration: BoxDecoration(
-                            image: DecorationImage(
-                              image: AssetImage('assets/images/light-1.png'),
-                            ),
+              ),
+              child: Stack(
+                alignment: Alignment.topCenter,
+                children: [
+                  Positioned(
+                    top: 0,
+                    left: 30,
+                    width: 80,
+                    height: 200,
+                    child: FadeInUp(
+                      duration: const Duration(milliseconds: 1000),
+                      child: Container(
+                        decoration: const BoxDecoration(
+                          image: DecorationImage(
+                            image: AssetImage('assets/images/light-1.png'),
                           ),
                         ),
                       ),
                     ),
-                    Positioned(
-                      left: 140,
-                      width: 80,
-                      height: 180,
-                      child: FadeInUp(
-                        duration: Duration(microseconds: 1200),
-                        child: Container(
-                          decoration: BoxDecoration(
-                              image: DecorationImage(
-                            image: AssetImage('assets/images/light-2.png'),
-                          )),
+                  ),
+                  Positioned(
+                    left: 140,
+                    width: 80,
+                    height: 150,
+                    child: FadeInUp(
+                      delay: const Duration(milliseconds: 1200),
+                      // duration: Duration(microseconds: 1200),
+                      child: Container(
+                        decoration: const BoxDecoration(
+                            image: DecorationImage(
+                          image: AssetImage('assets/images/light-2.png'),
+                        )),
+                      ),
+                    ),
+                  ),
+                  Positioned(
+                    top: 30,
+                    right: 10,
+                    width: 60,
+                    height: 60,
+                    child: FadeIn(
+                      delay: const Duration(milliseconds: 2000),
+                      // duration: Duration(microseconds: 1300),
+                      child: Container(
+                        decoration: const BoxDecoration(
+                            image: DecorationImage(
+                                image: AssetImage('assets/images/clock.png'))),
+                      ),
+                    ),
+                  ),
+                  Positioned(
+                    top: 100,
+                    child: FadeInUp(
+                      duration: const Duration(milliseconds: 1500),
+                      child: Container(
+                        margin: const EdgeInsets.only(top: 80),
+                        child: const Text(
+                          'Login',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 40,
+                            fontWeight: FontWeight.bold,
+                          ),
                         ),
                       ),
                     ),
-                    Positioned(
-                      top: 30,
-                      right: 10,
-                      width: 60,
-                      height: 60,
-                      child: FadeIn(
-                        duration: Duration(microseconds: 1300),
-                        child: Container(
-                          decoration: BoxDecoration(
-                              image: DecorationImage(
-                                  image:
-                                      AssetImage('assets/images/clock.png'))),
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
+                  ),
+                ],
               ),
-            ],
-          ),
+            ),
+            Container(
+              height: 350,
+              margin: const EdgeInsets.symmetric(horizontal: 20),
+              child: Column(
+                children: [
+                  FadeInUp(
+                    duration: const Duration(milliseconds: 1500),
+                    child: Container(
+                      padding: const EdgeInsets.all(5),
+                      decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(10),
+                          border: Border.all(
+                              color: const Color.fromRGBO(
+                            143,
+                            148,
+                            251,
+                            1,
+                          )),
+                          boxShadow: const [
+                            BoxShadow(
+                              color: Color.fromRGBO(143, 148, 251, .2),
+                              blurRadius: 20.0,
+                              offset: Offset(0, 10),
+                            )
+                          ]),
+                      child: Column(
+                        children: [
+                          Container(
+                            padding: const EdgeInsets.all(8.0),
+                            decoration: const BoxDecoration(
+                                border: Border(
+                                    bottom: BorderSide(
+                                        color:
+                                            Color.fromRGBO(143, 148, 251, 1)))),
+                            child: TextField(
+                              decoration: InputDecoration(
+                                  border: InputBorder.none,
+                                  hintText: "Email or Phone number",
+                                  hintStyle:
+                                      TextStyle(color: Colors.grey[700])),
+                            ),
+                          ),
+                          Container(
+                            padding: const EdgeInsets.all(8.0),
+                            child: TextField(
+                              obscureText: true,
+                              decoration: InputDecoration(
+                                  border: InputBorder.none,
+                                  hintText: "Password",
+                                  hintStyle:
+                                      TextStyle(color: Colors.grey[700])),
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 30,
+                  ),
+                  FadeInUp(
+                      duration: const Duration(milliseconds: 1900),
+                      child: Container(
+                        height: 50,
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(10),
+                            gradient: const LinearGradient(colors: [
+                              Color.fromRGBO(143, 148, 251, 1),
+                              Color.fromRGBO(143, 148, 251, .6),
+                            ])),
+                        child: const Center(
+                          child: Text(
+                            "Login",
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold),
+                          ),
+                        ),
+                      )),
+                  const SizedBox(
+                    height: 70,
+                  ),
+                  FadeInUp(
+                    duration: const Duration(milliseconds: 2000),
+                    child: const Text(
+                      "Forgot Password?",
+                      style: TextStyle(
+                        color: Color.fromRGBO(143, 148, 251, 1),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
         ),
       ),
     );
